42f9dae23e248bfcefea988dedb27757e17a88f4
Nicolas <nicolas.estebe@agentil.com>
2021-09-20 11:52:37 +0200

    add tests

--- test.js
+++ test.js
@@ -2,11 +2,15 @@ const { FrenchSentimentAnalyzer, Sentiment } = require("./lib/index.js")
 
 
 test("test positive sentiment", () => {
+
   expect(FrenchSentimentAnalyzer.getSentiment("ce programme est super")).toBeGreaterThan(0)
+  expect(FrenchSentimentAnalyzer.getSentiment("ce programme est super")).toBe(Sentiment.POSITIVE)
+
 
 })
 
 test("test negative sentiment", () => {
   expect(FrenchSentimentAnalyzer.getSentiment("ce programme est très mauvais")).toBeLessThan(0)
+  expect(FrenchSentimentAnalyzer.getSentiment("ce programme est très mauvais")).toBe(Sentiment.NEGATIVE)
 })
 
