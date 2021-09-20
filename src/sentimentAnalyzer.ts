0ee456ee1c731cb7e5b479953b6d3ca0052ce9d1
Nicolas <nicolas.estebe@agentil.com>
2021-09-20 11:53:08 +0200

    refactor

--- src/sentimentAnalyzer.ts
+++ src/sentimentAnalyzer.ts
@@ -6,19 +6,20 @@ import { Sentiment } from './enums'
 const tokenizer = new AggressiveTokenizerFr();
 const analyzer = new SentimentAnalyzer("French", PorterStemmerFr, "pattern")
 
-function getSentiment(sentence: string): Sentiment {
-    if (!sentence.trim()) {
-        return Sentiment.NEUTRAL
+
+export class FrenchSentimentAnalyzer {
+
+    static getSentiment(sentence: string): Sentiment {
+        if (!sentence.trim()) {
+            return Sentiment.NEUTRAL
+        }
+
+        //remove special caracters
+        const tokenized = tokenizer.tokenize(sentence)
+        //remove stop words
+        const stopWordsRemoved = stopword.removeStopwords(tokenized)
+
+        return analyzer.getSentiment(stopWordsRemoved) < 0 ? Sentiment.NEGATIVE : Sentiment.POSITIVE
     }
 
-    //remove special caracters
-    const tokenized = tokenizer.tokenize(sentence)
-    //remove stop words
-    const stopWordsRemoved = stopword.removeStopwords(tokenized)
-
-    return analyzer.getSentiment(stopWordsRemoved) < 0 ? Sentiment.NEGATIVE : Sentiment.POSITIVE
-}
-
-module.exports = {
-    getSentiment: getSentiment,
 }
